30f6c041766dd9857ce9567c7c1fe1153ce46392
sedupuganti <sedupuganti@tamu.edu>
2021-10-16 15:42:37 -0500

    Viewer beware feature added

--- src/Recommended.java
+++ src/Recommended.java
@@ -1,21 +1,29 @@
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.View;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Recommended extends JFrame {
 
     private JPanel recommendedPanel;
     private JLabel titleLabel;
-    private JTable recTable;
     private JButton nextButton;
     private JButton searchButton;
     private JButton backButton;
     private JButton back_home;
-    private JScrollBar scrollBar1;
+    private JTable table1;
+    private JButton viewerBeware;
+    public String[] columnNames;
+    public DefaultTableModel model;
 
-    public Recommended()
-    {
+    public Recommended() {
         super();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(recommendedPanel);
@@ -53,6 +61,177 @@ public class Recommended extends JFrame {
                 Recommended.this.dispose();
             }
         });
+        viewerBeware.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                ViewerBeware vb = new ViewerBeware();
+                vb.setVisible(true);
+                Recommended.this.dispose();
+
+            }
+        });
     }
 
+    private void createUIComponents() {
+        List<Object[]> list = new ArrayList<Object[]>();
+        List<Object[]> list2 = new ArrayList<Object[]>();
+
+        dbSetup my = new dbSetup();
+        //Building the connection
+        Connection conn = null;
+        Connection conn1 = null;
+
+
+        try {
+            //Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315_914_5_db",
+                    my.user, my.pswd);
+        } catch (Exception f) {
+            f.printStackTrace();
+            System.err.println(f.getClass().getName()+": "+f.getMessage());
+            System.exit(0);
+        }//end try catch
+        System.out.println("Opened database successfully");
+        String cus_lname = "";
+        //  String username = "";
+        try{
+            //create a statement object
+            Statement stmt = conn.createStatement();
+            //create an SQL statement
+            String sqlStatement = (
+                    "SELECT genres FROM titles INNER JOIN " +
+                            "customer_ratings ON titles.title_id = " +
+                            "customer_ratings.title_id AND customer_ratings.customer_id = " +
+                            "'1488844' ORDER BY customer_ratings.rating DESC LIMIT 30;"
+            );
+
+            System.out.println(sqlStatement);
+            //send statement to DBMS
+            ResultSet result = stmt.executeQuery(sqlStatement);
+
+            //OUTPUT
+            System.out.println("Database");
+            System.out.println("______________________________________");
+
+            while (result.next()) {
+
+                //System.out.println("GETTING HERE 1!");
+
+                //String titleName = result.getString("original_title");
+                //String year = result.getString("start_year");
+                String genre = result.getString("genres");
+                //String avgRev = result.getString("average_rating");
+                //String runtime = result.getString("runtime_minutes");
+
+                //System.out.println("GETTING HERE 2!");
+
+                //String dateWatched = result.getString("date_posted");
+
+                //System.out.println("GETTING HERE 3!");
+
+                //Object[] information = {titleName, year, genre, avgRev, runtime, dateWatched};
+                Object[] information = new Object[1];
+
+                if (genre.contains(",")) {
+                    String[] parts = genre.split(",");
+                    for (int i = 0; i < parts.length; i++ ) {
+                        information[0] = parts[i];
+                        list.add(information);
+                        //System.out.println(information[0]);
+                    }
+                }
+                else { // no comma
+                    information[0] = genre;
+                    list.add(information);
+                    //System.out.println(information[0]);
+                }
+
+
+
+                //System.out.println("GETTING HERE 4!");
+
+                //list.add(information);
+
+            }
+            stmt.close();
+
+        } catch (Exception f){
+            System.out.println("Error accessing Database.");
+
+        }
+        // take list of genres and iterate through
+        System.out.println(list.get(0)[0]);
+
+        // iterating through all of the genres
+
+        for (int m = 0; m < list.size(); m++) {
+
+
+            //System.out.println("Opened database successfully");
+            //String cus_lname1 = "";
+            //  String username = "";
+            try{
+                //create a statement object
+                Statement stmt1 = conn.createStatement();
+                //create an SQL statement
+                String sqlStatement1 = (
+                        "SELECT DISTINCT original_title, start_year, genres, average_rating," +
+                                " runtime_minutes FROM titles INNER JOIN customer_ratings ON " +
+                                "titles.title_id = customer_ratings.title_id AND titles.genres " +
+                                "LIKE '%"+ list.get(m)[0] +"%'LIMIT 5;"
+                );
+
+                //System.out.println(sqlStatement1);
+                //send statement to DBMS
+                ResultSet result1 = stmt1.executeQuery(sqlStatement1);
+
+                //OUTPUT
+                //System.out.println("Database");
+                //System.out.println("______________________________________");
+
+                while (result1.next()) {
+
+                    //System.out.println("GETTING HERE 1!");
+
+                    String titleName = result1.getString("original_title");
+                    String year = result1.getString("start_year");
+                    String genre = result1.getString("genres");
+                    String avgRev = result1.getString("average_rating");
+                    String runtime = result1.getString("runtime_minutes");
+
+                    //System.out.println("GETTING HERE 2!");
+
+                    //String dateWatched = result1.getString("date_posted");
+
+                    //System.out.println("GETTING HERE 3!");
+
+                    Object[] information = {titleName, year, genre, avgRev, runtime};
+
+                    //System.out.println("GETTING HERE 4!");
+
+                    list2.add(information);
+
+                }
+
+            } catch (Exception f){
+                System.out.println("Error accessing Database.");
+            }
+
+        }
+
+
+        Object[][] data = list2.toArray(new Object[list.size()][5]);
+
+        System.out.println("Hello1");
+        columnNames = new String[]{"Title", "Year", "Genre", "Avg Review", "Runtime"};
+        model = new DefaultTableModel(data, columnNames);
+
+        table1 = new JTable(model);
+        table1.setFillsViewportHeight(true);
+        table1.setEnabled(false);
+
+
+        // TODO: place custom component creation code here
+    }
 }
---
+++ src/ViewerBeware.java
@@ -0,0 +1,214 @@
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.text.View;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewerBeware extends JFrame {
+    private JPanel viewerBewarePanel;
+    public JTable vbTable;
+    private JButton back_home;
+    private JButton searchButton;
+    private JButton backButton;
+    public String[] columnNames;
+    public DefaultTableModel model;
+
+    public ViewerBeware() {
+        super();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(viewerBewarePanel);
+        this.pack();
+
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Recommended rec = new Recommended();
+                rec.setVisible(true);
+                ViewerBeware.this.dispose();
+            }
+        });
+
+        back_home.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Home home = new Home("Home page");
+                home.setVisible(true);
+                ViewerBeware.this.dispose();
+            }
+        });
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+
+        ArrayList<String> list = new ArrayList<String>();
+        List<Object[]> finalOutput = new ArrayList<>();
+
+        dbSetup my = new dbSetup();
+        //Building the connection
+        Connection conn = null;
+
+        try {
+            //Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(
+                    "jdbc:postgresql://csce-315-db.engr.tamu.edu/csce315_914_5_db",
+                    my.user, my.pswd);
+        } catch (Exception f) {
+            f.printStackTrace();
+            System.err.println(f.getClass().getName()+": "+f.getMessage());
+            System.exit(0);
+        }//end try catch
+        System.out.println("Opened database successfully");
+        String cus_lname = "";
+        //  String username = "";
+        try{
+            //create a statement object
+            Statement stmt = conn.createStatement();
+            //create an SQL statement
+            String sqlStatement = (
+                    "SELECT DISTINCT customer_id " +
+                            "FROM customer_ratings;"
+            );
+
+            System.out.println(sqlStatement);
+            //send statement to DBMS
+            ResultSet result = stmt.executeQuery(sqlStatement);
+
+            //OUTPUT
+            System.out.println("Database");
+            System.out.println("______________________________________");
+
+            while (result.next()) {
+
+                String input = result.getString("customer_id");
+                if (!input.equals(Login.userInfo)) {
+                    System.out.println(input);
+                    list.add(input);
+                }
+
+
+                System.out.println("CHECK!");
+
+            }
+
+            System.out.println("CHECK 2!");
+
+            String avoidUser = "";
+            int greatDislikeSim = 0;
+
+            for (int i = 0; i < list.size(); i++) {
+
+                    String sqlStatement2 = (
+                            "SELECT COUNT(title_id) AS num_similar " +
+                                    "FROM (SELECT title_id " +
+                                    "FROM customer_ratings " +
+                                    "WHERE customer_id = '" + Login.userInfoInt + "' AND rating < 3 " +
+                                    "INTERSECT " +
+                                    "SELECT title_id " +
+                                    "FROM customer_ratings " +
+                                    "WHERE customer_id = '" + list.get(i) + "' AND rating < 3) I;"
+                    );
+
+                    System.out.println(sqlStatement2);
+
+                    ResultSet result2 = stmt.executeQuery((sqlStatement2));
+
+                    while (result2.next()) {
+
+                        System.out.println("CHECK 3!");
+
+                        String dislikesCommon = result2.getString("num_similar");
+                        int dislikesCommonInt = Integer.parseInt(dislikesCommon);
+
+                        System.out.println("CHECK 4!");
+
+
+                        if (dislikesCommonInt > greatDislikeSim) {
+                            greatDislikeSim = dislikesCommonInt;
+                            avoidUser = list.get(i);
+                        }
+
+                        System.out.println("CHECK 5!");
+
+                    }
+
+
+
+
+            }
+
+            System.out.println("CHECK 6!");
+
+            System.out.println("Use with similar dislikes is " + avoidUser);
+            System.out.println("This user has " + greatDislikeSim + " similarities!");
+
+
+            String sqlStatement3 = (
+                    "SELECT original_title, start_year, genres, average_rating, runtime_minutes " +
+                            "FROM titles " +
+                            "INNER JOIN customer_ratings " +
+                            "ON titles.title_id = customer_ratings.title_id " +
+                            "WHERE customer_ratings.customer_id = '" + avoidUser + "' " +
+                            "AND rating < 3 " +
+                            "AND titles.title_id " +
+                            "NOT IN " +
+                            "(SELECT title_id " +
+                                "FROM customer_ratings " +
+                                "WHERE customer_id = '" + Login.userInfoInt + "' " +
+                                "AND rating < 3  INTERSECT SELECT title_id " +
+                                "FROM customer_ratings " +
+                                "WHERE customer_id = '" + avoidUser + "' " +
+                                "AND rating < 3);"
+            );
+
+            ResultSet result3 = stmt.executeQuery(sqlStatement3);
+
+            System.out.println(sqlStatement3);
+
+            System.out.println("CHECK 7!");
+
+
+            while (result3.next()) {
+
+
+                String titleName = result3.getString("original_title");
+                String year = result3.getString("start_year");
+                String genre = result3.getString("genres");
+                String avgRev = result3.getString("average_rating");
+                String runtime = result3.getString("runtime_minutes");
+
+                System.out.println("GETTING HERE 2!");
+
+                System.out.println("GETTING HERE 3!");
+
+                Object[] information = {titleName, year, genre, avgRev, runtime};
+
+                System.out.println("GETTING HERE 4!");
+
+                finalOutput.add(information);
+
+
+            }
+
+        } catch (Exception f){
+            System.out.println("Error accessing Database.");
+        }
+
+        Object[][] data = finalOutput.toArray(new Object[list.size()][5]);
+
+
+//        String[] columnNames = {"Title", "Year", "Genre", "Avg Review", "Runtime"};
+        columnNames = new String[]{"Title", "Year", "Genre", "Avg Review", "Runtime"};
+        model = new DefaultTableModel(data, columnNames);
+
+        vbTable = new JTable(model);
+        vbTable.setFillsViewportHeight(true);
+        vbTable.setEnabled(false);
+    }
+}
