370851ae2260a932129dd6b8c3cb79acaa750962
Sidharth <sidharth.edupuganti@gmail.com>
2021-12-31 14:27:51 -0800

    removed user/pass

--- src/dbSetup.java
+++ src/dbSetup.java
@@ -4,6 +4,6 @@ Database setup example
 Purpose: Keeping usernames and password private.
 */
 public final class dbSetup  {
-  public static final String user = "***REMOVED***";
-  public static final String pswd = "***REMOVED***";
+  public static final String user = "";
+  public static final String pswd = "";
 }//end class
